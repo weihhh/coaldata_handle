b0c6849522a1254fa23d45cc908dc7b2f233e785
weber <wz591757596@163.com>
2017-05-05 10:53:21 +0800

    重写数据打开和选择函数，效率更高，不易出错

--- re_coal/re_coal.py
+++ re_coal/re_coal.py
@@ -13,11 +13,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
-
-  
-
 def pre_proceed(data):
     pjz=np.mean(data)
     bzc=np.std(data)
@@ -37,45 +32,43 @@ class Application(Frame):
     def __init__(self,master=None):
         Frame.__init__(self,master)
         self.pack()
-        self.openfilename=self.get_file()
-        self.filename=StringVar()
+        self.filename=None
+        self.filename_lable=StringVar()
         self.up_value=IntVar()
         self.down_value=IntVar()
         self.corr_multi=IntVar()
         self.createWidgets()
     
-    def get_file(self):
-        self.openfilename=tkinter.filedialog.askopenfilename(defaultextension=".csv",filetypes = [("csv文件",".csv")])#filetypes将对文件进行筛选显示，askopenfilename返回的是字符串
-            
+        
     
-    def createWidgets(self):        
+    def createWidgets(self):#界面布局        
         
         
-        self.noteLabel1 = Label(self, text='1.select the filename !')
+        self.noteLabel1 = Label(self, text='1.点击打开选择数据文件!')
         self.noteLabel1.grid(row=0,sticky=W)#提示测试名称标签
         
-        self.confirmButton1 = Button(self, text='open', command=self.get_file)
+        self.confirmButton1 = Button(self, text='打开', command=self.get_file)
         self.confirmButton1.grid(row=0,column=1)#确认测试文件名按钮
         
         
         self.noteLabel3=Label(self)
-        self.noteLabel3['textvariable']=self.filename        
+        self.noteLabel3['textvariable']=self.filename_lable        
         self.noteLabel3.grid(row=2,sticky=W)#显示当前读取文件名
         
-        self.noteLabel4=Label(self,text='3.choose up or down datapath,only one')
+        self.noteLabel4=Label(self,text='3.选择上下游通道，仅可选一路')
         self.noteLabel4.grid(row=3,sticky=W)
-        self.up_choose=Checkbutton(self,text='choose up ',variable=self.up_value)
-        self.down_choose=Checkbutton(self,text='choose down ',variable=self.down_value)
+        self.up_choose=Checkbutton(self,text='上路',variable=self.up_value)
+        self.down_choose=Checkbutton(self,text='下路',variable=self.down_value)
         self.up_choose.grid(row=3,column=1)
         self.down_choose.grid(row=3,column=2)
         
-        self.noteLabel5 = Label(self, text='4.input the row you want !')
+        self.noteLabel5 = Label(self, text='4.选择需要显示的组数!')
         self.noteLabel5.grid(row=4,sticky=W)#提示标签
         self.start_rowInput = Entry(self)
         self.start_rowInput.grid(row=5)#输入框
         self.end_rowInput = Entry(self)
         self.end_rowInput.grid(row=5,column=1)#输入框
-        self.confirmButton2 = Button(self, text='ok', command=self.make_pic)
+        self.confirmButton2 = Button(self, text='绘图', command=self.make_pic)
         self.confirmButton2.grid(row=5,column=2)#确认输入按钮
         
         self.noteLabel6 = Label(self, text='5.input the row you want to do corr!')
@@ -89,25 +82,36 @@ class Application(Frame):
         self.confirmButton3 = Button(self, text='corr', command=self.my_corr)
         self.confirmButton3.grid(row=7,column=3)#确认输入按钮
         
-        
-        
-        
-        
-        self.noteLabel7 = Label(self, text='welcome!')
+        self.noteLabel7 = Label(self, text='fuck coal!')
         self.noteLabel7.grid(row=8,sticky=W)#提示标签
-        
-        # self.backButton = Button(self, text='back', command=self.setvar)
-        # self.backButton.grid(row=6,column=0)#确认输入按钮
-        # self.nextButton = Button(self, text='next', command=self.setvar)
-        # self.nextButton.grid(row=6,column=1)#确认输入按钮
+            
+    def get_file(self):#打开文件获取初始数据
+        self.filename=tkinter.filedialog.askopenfilename(defaultextension=".csv",filetypes = [("csv文件",".csv")])#filetypes将对文件进行筛选显示，askopenfilename返回的是字符串
+        self.filename_lable.set(self.filename)
+        self.ori_data=pd.read_csv(self.filename,error_bad_lines=False,index_col=[0,1],header=None,skiprows=[0])
+        del_long=lambda x: x%10000#若碰到四位四位相连的数据，保留后四位
+        self.pro_data=self.ori_data.fillna(method='ffill',axis=1).ix[:,0:5].applymap(del_long)#这里因为将1，2作为index，所以默认就是显示的，ix[:,y:4]中的y不管取0，1都是一样的，共0-5，0-1索引，2-5数据
+        print(self.pro_data)
+
+    def make_pic(self):
+        #从这开始做根据设置的范围取出数据
+        start_times=int(self.start_rowInput.get() or '1')
+        end_times=int(self.end_rowInput.get()) if self.end_rowInput.get() else start_times
+        start_series=pd.Series()
+        if self.up_value.get()==1 and self.down_value.get()!=1 :        
+            plot_data_series=start_series.append([self.get_row(i,'u') for i in range(start_times,end_times+1)],ignore_index=True)
+        if self.down_value.get()==1:
+            plot_data_series=start_series.append([self.get_row(i,'u') for i in range(start_times,end_times+1)],ignore_index=True)
+
+        plot_data_series.plot()
+        plt.show()
     
-    
-        
-    def set_filename(self):
-        full_current_file=get_full_path(self.testname,self.testnumber)
-        self.filename.set(full_current_file)
-        self.data=pd.read_csv(full_current_file,index_col=0,header=None)#关键词变量：sep(str or default ','),index_col代表第几列作为行名，设置列名就同时设置header=None
-        
+    def get_row(self,times,updown):
+        row=self.pro_data.ix[times,:5].ix[updown]#return series,非副本，只是视图
+        #self.datalen=len(row)
+        return row   
+
+                
     def my_corr(self):
         #做互相关，并显示原始数据，处理后数据，相关值获取
         
@@ -177,57 +181,7 @@ class Application(Frame):
             plt.show()
 
         
-    
-    
-    def make_pic(self):
-        #从这开始做根据设置的范围取出数据
-        start_times=int(self.start_rowInput.get() or '1')
-        if self.up_value.get()==1 and self.down_value.get()!=1 :
-            
-            self.current_up_row=self.get_row(start_times,'u')
-        if self.down_value.get()==1:
-            self.current_up_row=self.get_row(start_times,'d')
-        #self.previous_row=self.current_row.copy()
-        plot_data_array=self.current_up_row#self.current_row=pd.Series(self.current_row)这样子的程序没有用，无法改变类型
-        #self.current_up_row=b
-        #print(type(self.current_row))
-        if self.end_rowInput.get():
-            end_times=int(self.end_rowInput.get())
-            if self.up_value.get()==1 and self.down_value.get()!=1:            
-                for i in range(start_times+1,end_times+1):
-                    self.current_down_row=self.get_row(i,'u')
-                    plot_data_array=np.concatenate((plot_data_array,self.current_down_row))#注意这里需要结合的两个array需要在一个括号里面
-            if self.down_value.get()==1:
-                for i in range(start_times+1,end_times+1):
-                    self.current_down_row=self.get_row(i,'d')
-                    plot_data_array=np.concatenate((plot_data_array,self.current_down_row))#注意这里需要结合的两个array需要在一个括号里面
-        #完成取出数据plot_data_array    
-                               
-        plot_data=pd.Series(plot_data_array)
-        plot_data.plot()
-        plt.show()
-    
-    def get_row(self,times,updown):
-        if updown=='u':
-            times=2*times-2
-        else:
-            times=2*times-1
-        row=self.data.iloc[times].values
-        #print(row)
-        datalen=len(row)
-        print(datalen)
-        row=self.data.iloc[times,:datalen-1].values.copy()#iloc根据位置选取，【行，列】
-        #print(type(row))
-        print(row)
-        return row    
-    
-    def setvar(self):
-        
-        self.testname = self.testnameInput.get()
-        self.testnumber = self.testnumberInput.get() or self.testnumber
-        print(self.up_value.get())#checkbutton的值为0或1，通过绑定一个intvar到本身变量
-        self.filename.set(self.testname+self.testnumber)
-        
+ 
 
 app = Application()
 # 设置窗口标题:
--- re_coal/read_file.py
+++ re_coal/read_file.py
@@ -14,6 +14,6 @@ def get_file():
 df=pd.read_csv('testdata1.csv',error_bad_lines=False,index_col=[0,1],header=None,skiprows=[0])
 del_long=lambda x: x%10000#若碰到四位四位相连的数据，保留后四位
 df_fill=df.fillna(method='ffill',axis=1).ix[:,0:5].applymap(del_long)#这里因为将1，2作为index，所以默认就是显示的，ix[:,y:4]中的y不管取0，1都是一样的，共0-5，0-1索引，2-5数据
-print(df,'\n',df_fill)
-df_fill.ix[3,:4].ix['d']#选取第3组数据的d路
+print(df,'\n',df_fill.ix[3,:5].ix['d'])
+print(len(df_fill.ix[3,:5].ix['d']))#选取第3组数据的d路,series        
 
